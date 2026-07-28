90f5cd71cc8f7f1e9341d31bc40a9519f85841b5
T Savo <tsavo@users.noreply.github.com>
2026-07-28 15:29:11 -0700

    Own encoded string subscript mutations

--- implementations/python/sugar-lift-py-tests/src/sugar_lift_py_tests/floor/encoded_string_value.py
+++ implementations/python/sugar-lift-py-tests/src/sugar_lift_py_tests/floor/encoded_string_value.py
@@ -21,5 +21,25 @@ class EncodedStringValue(FloorValue):
     table: tuple[int, ...]
     indices: tuple[Term, ...]
 
+    def setitem(self, index, value, site):
+        del index, value
+        from sugar_lift_py_tests.floor.ground_exit import ground_exceptional_exit
+
+        return ground_exceptional_exit(
+            exception_name="TypeError",
+            site=site,
+            owner="EncodedStringValue.setitem",
+        )
+
+    def delitem(self, index, site):
+        del index
+        from sugar_lift_py_tests.floor.ground_exit import ground_exceptional_exit
+
+        return ground_exceptional_exit(
+            exception_name="TypeError",
+            site=site,
+            owner="EncodedStringValue.delitem",
+        )
+
     def binary_operator_with(self, operation, ctx):
         return operation.binary_encoded_string(self, ctx)
