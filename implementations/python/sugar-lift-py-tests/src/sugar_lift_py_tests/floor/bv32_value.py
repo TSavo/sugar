d08dda2b045024316eea8922c212a0225a099a4c
T Savo <tsavo@users.noreply.github.com>
2026-07-28 16:49:59 -0700

    Own bv32 subscript mutations

--- implementations/python/sugar-lift-py-tests/src/sugar_lift_py_tests/floor/bv32_value.py
+++ implementations/python/sugar-lift-py-tests/src/sugar_lift_py_tests/floor/bv32_value.py
@@ -11,6 +11,22 @@ from .floor_value import FloorValue
 class Bv32Value(FloorValue):
     term: Term
 
+    def setitem(self, index, value, site):
+        del index, value
+        from sugar_lift_py_tests.floor.ground_exit import ground_exceptional_exit
+
+        return ground_exceptional_exit(
+            exception_name="TypeError", site=site, owner="Bv32Value.setitem"
+        )
+
+    def delitem(self, index, site):
+        del index
+        from sugar_lift_py_tests.floor.ground_exit import ground_exceptional_exit
+
+        return ground_exceptional_exit(
+            exception_name="TypeError", site=site, owner="Bv32Value.delitem"
+        )
+
     def to_term(self, *, owner: str):
         del owner
         return self.term
